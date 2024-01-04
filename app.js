00fecc3d81eb350c7c4288cedf702f422902648b
RodriguezJose92 <joserodriguez@mudi.com.co>
2024-01-04 15:59:57 +0000

    Actualizacion app.js

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.use(cors());
 app.use('/api/mudiV1', router);
 
 https.createServer({
-    key:        fs.readFileSync('/etc/letsencrypt/live/mudiview.mudi.com.co/privkey.pem'),      // Ruta al archivo de clave privada
-    cert:       fs.readFileSync('/etc/letsencrypt/live/mudiview.mudi.com.co/cert.pem'),         // Ruta al archivo de certificado público
-    ca:         fs.readFileSync('/etc/letsencrypt/live/mudiview.mudi.com.co/chain.pem')         // Ruta al archivo de la autoridad de certificación (opcional)
-  }, app.listen(PORT, () => {
+    key:        fs.readFileSync('/etc/letsencrypt/live/visor.mudi.com.co/privkey.pem'),      // Ruta al archivo de clave privada
+    cert:       fs.readFileSync('/etc/letsencrypt/live/visor.mudi.com.co/cert.pem'),         // Ruta al archivo de certificado público
+    ca:         fs.readFileSync('/etc/letsencrypt/live/visor.mudi.com.co/chain.pem')         // Ruta al archivo de la autoridad de certificación (opcional)
+  }, app.listen(puerto, () => {
     console.log('El servidor está escuchando en el puerto ' + puerto);
 }));
 
