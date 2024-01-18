0ac3a5b24564622e3ecf4d3589aa2487910e6918
RodriguezJose92 <joserodriguez@mudi.com.co>
2024-01-18 01:09:14 -0500

    nueva actualización de mi rama

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const express       = require('express');
-const cors          = require('cors');
-const app           = express();
-const router        = require('./src/router');
-const https         = require('https');
-const fs            = require('fs');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const router = require('./src/router');
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config();
 
 // Config Puerto y listening
 const puerto = process.env.PORT;
 
-// Usamos JSON 
+// Usamos JSON
 app.use(express.json());
-// Usamos CORS 
+// Usamos CORS
 app.use(cors());
 
 app.use('/api/mudiV1', router);
 
-https.createServer({
-    key:        fs.readFileSync('/etc/letsencrypt/live/visor.mudi.com.co/privkey.pem'),      // Ruta al archivo de clave privada
-    cert:       fs.readFileSync('/etc/letsencrypt/live/visor.mudi.com.co/cert.pem'),         // Ruta al archivo de certificado público
-    ca:         fs.readFileSync('/etc/letsencrypt/live/visor.mudi.com.co/chain.pem')         // Ruta al archivo de la autoridad de certificación (opcional)
-  }, app.listen(puerto, () => {
-    console.log('El servidor está escuchando en el puerto ' + puerto);
-}));
+// Configuración del servidor HTTPS
+const opcionesHttps = {
+    key: fs.readFileSync('/etc/letsencrypt/live/viewer.mudi.com.co/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/viewer.mudi.com.co/cert.pem'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/viewer.mudi.com.co/chain.pem')
+};
 
-app.listen(puerto, () => {
-  console.log('El servidor está escuchando en el puerto ' + puerto);
+// Crear el servidor HTTPS, pero no escuchar en este punto
+const servidorHttps = https.createServer(opcionesHttps, app);
+
+// Ahora, escucha en el puerto especificado
+servidorHttps.listen(puerto, () => {
+    console.log('El servidor está escuchando en el puerto ' + puerto + ' con HTTPS');
 });
